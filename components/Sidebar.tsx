df3b474e0e047d277c359d0d02e152fcd718cad1
Sayantan Pal <sayantanrabi.03@gmail.com>
2024-10-15 00:09:54 +0530

    implement a little of sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,27 +1,65 @@
+'use client'
+
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 interface SidebarProps {
-  user: any; // Define user type as needed
+    user: any; // Define user type as needed
 }
 
 const Sidebar = ({ user }: SidebarProps) => {
-  return (
-    <section className="sidebar">
-      <nav className="flex flex-col gap-4">
-        <Link href="/"  className="mb-12 cursor-pointer items-center gap-2">
-            <Image
-              src="/icons/logo.svg"
-              width={34}
-              height={34}
-              alt="Logo"
-            />
-        
-        </Link>
-      </nav>
-    </section>
-  );
+    const pathname = usePathname();
+
+    return (
+        <section className="sidebar">
+            <nav className="flex flex-col gap-4">
+                <Link href="/" className="mb-12 flex items-center gap-2 cursor-pointer">
+                    <Image
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt="Horizon Logo"
+                        className="w-[24px] max-xl:w-12 flex"
+                    />
+                    <h1 className="sidebar-logo">Horizon</h1>
+                </Link>
+
+                {sidebarLinks.map((item) => {
+                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+                    return (
+                        <Link
+                            href={item.route}
+                            key={item.label}
+                            className={cn('sidebar-link', { 'bg-bank-gradient': isActive }, { 'active': isActive })} // Add 'active' class if the link is active
+                        >
+                            <div className='relative size-6'>
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
+                                    className={cn({ 'brightness-[3] invert-0': isActive })}
+                                />
+
+                            </div>
+                            <p className={cn('sidebar-label', { '!text-white': isActive })}>
+                                {item.label}
+                            </p>
+
+                        </Link>
+                    );
+                })}
+            
+
+                USER
+            </nav>
+
+            FOOTER
+        </section>
+    );
 };
 
 export default Sidebar;
