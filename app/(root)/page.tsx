6509c6514b2ec0ddfcc1fa20240de685f378906d
Sayantan Pal <sayantanrabi.03@gmail.com>
2024-10-16 00:03:02 +0530

    implement the irght side bar a little

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
@@ -23,8 +24,14 @@ const Home = () => {
             totalCurrentBalance={1250.35}
             />
         </header>
+      RECENT TRANSACTIONS
 
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+        />
     </section>
   )
 }
